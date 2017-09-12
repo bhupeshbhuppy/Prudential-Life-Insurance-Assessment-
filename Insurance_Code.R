a40e1d94f0bdc1105bdad784deeb7557915172f8
bhupeshbhuppy <bhupesh.bhuppy@gmail.com>
2017-09-13 01:21:37 +0530

    Changing charts. Incomplete . Do look into nplots from rCharts

--- Insurance_Code.R
+++ Insurance_Code.R
@@ -17,6 +17,7 @@ library(pROC)
 library(performanceEstimation)
 library(rCharts)
 library(highcharter)
+library (qcc)
 inputfolder<-paste0(getwd(),"/input/")
 outputfolder<-paste0(getwd(),"/output/")
 train = read.csv(paste0(inputfolder,"train.csv"))
@@ -116,27 +117,28 @@ plt
 
 ### Density Plots
 
-hcdensity(train_conti[,1], area = TRUE, name= colnames(train_conti)[1])%>%
-  hc_add_series(density(train_conti[,2]), area = TRUE, name= colnames(train_conti)[2])
+hcdensity(train_conti[,1], type= "area", name= colnames(train_conti)[1])%>%
+  hc_add_series(density(train_conti[,2]), type= "area", name= colnames(train_conti)[2])
 
-hcdensity(train_conti[,3], area = TRUE, name= colnames(train_conti)[3])%>%
-  hc_add_series(density(train_conti[,4]), area = TRUE, name= colnames(train_conti)[4])%>%
-  hc_add_series(density(train_conti[,5]), area = TRUE, name= colnames(train_conti)[5])
+hcdensity(train_conti[,3], type= "area", name= colnames(train_conti)[3])%>%
+  hc_add_series(density(train_conti[,4]), type= "area", name= colnames(train_conti)[4])%>%
+  hc_add_series(density(train_conti[,5]), type= "area", name= colnames(train_conti)[5])
 
-hcdensity(train_conti[,6], area = TRUE, name= colnames(train_conti)[6])%>%
-  hc_add_series(density(train_conti[,8]), area = TRUE, name= colnames(train_conti)[8])
+hcdensity(train_conti[,6], type= "area", name= colnames(train_conti)[6])%>%
+  hc_add_series(density(train_conti[,8]), type= "area", name= colnames(train_conti)[8])
 
-hcdensity(train_conti[,7], area = TRUE, name= colnames(train_conti)[7])
+hcdensity(train_conti[,7], type= "area", name= colnames(train_conti)[7])
 
 ### Missing Value
-
-par(mfrow=c(2,2))
+index<-1
+plt<-list()
 for(i in var_kind){
-  plot(x=as.factor(as.character(missing_prct[grep(i, row.names(missing_prct)),1])),
-       y=missing_prct$missing[grep(i, row.names(missing_prct))], ylim = c(0,1),
-       main =gsub("_"," ",i))
-  
+  p<-nPlot(missing ~ variable,data = missing_prct[grep(i, row.names(missing_prct)),],
+        type = "multiBarChart")
+  plt[[index]]<-p
+  index<-index+1
 }
+plt
 ### Missing vs Response Chart
 train_na_response <- sapply(sort(unique(train$Response)), function(x) { 
   apply(train[train$Response == x, ], 2, function(y) { sum(is.na(y)) }) })
@@ -150,16 +152,197 @@ nPlot(value ~ ID, group = "variable", data = train_na_response_melt,
 ### Event Rate Charts
 
 train_categ<-train_clean[,-which(colnames(train_clean) %in% colnames(train_conti))]
-nPlot(Freq ~ Var1, group = "Var2", data = data_freq,
-      type = "multiBarChart")
 i="Product_Info"
 train_temp<-train_categ[,grep(i,colnames(train_categ))]
 index<-1
-plt<-c()
+plt<-list()
 for (i in colnames(train_temp)){
   data_freq<-as.data.frame(table(train_temp[,i],train_clean$Response)/(as.data.frame(table(train_temp[,i]))[,2]))
   nPlot(Freq ~ Var1, group = "Var2", data = data_freq,
            type = "multiBarChart")
   #p
+  plt[[index]]<-p
+  index<-index+1
 }
 plt
+
+i="Employment_Info"
+train_temp<-train_categ[,grep(i,colnames(train_categ))]
+index<-1
+plt<-list()
+for (i in colnames(train_temp)){
+  data_freq<-as.data.frame(table(train_temp[,i],train_clean$Response)/(as.data.frame(table(train_temp[,i]))[,2]))
+  nPlot(Freq ~ Var1, group = "Var2", data = data_freq,
+        type = "multiBarChart")
+  #p
+  plt[[index]]<-p
+  index<-index+1
+}
+plt
+
+i="InsuredInfo"
+train_temp<-train_categ[,grep(i,colnames(train_categ))]
+index<-1
+plt<-list()
+for (i in colnames(train_temp)){
+  data_freq<-as.data.frame(table(train_temp[,i],train_clean$Response)/(as.data.frame(table(train_temp[,i]))[,2]))
+  nPlot(Freq ~ Var1, group = "Var2", data = data_freq,
+        type = "multiBarChart")
+  #p
+  plt[[index]]<-p
+  index<-index+1
+}
+plt
+i="Insurance_History"
+train_temp<-train_categ[,grep(i,colnames(train_categ))]
+index<-1
+plt<-list()
+for (i in colnames(train_temp)){
+  data_freq<-as.data.frame(table(train_temp[,i],train_clean$Response)/(as.data.frame(table(train_temp[,i]))[,2]))
+  nPlot(Freq ~ Var1, group = "Var2", data = data_freq,
+        type = "multiBarChart")
+  #p
+  plt[[index]]<-p
+  index<-index+1
+}
+plt
+i="Medical_History"
+train_temp<-train_categ[,grep(i,colnames(train_categ))]
+index<-1
+plt<-list()
+for (i in colnames(train_temp)){
+  data_freq<-as.data.frame(table(train_temp[,i],train_clean$Response)/(as.data.frame(table(train_temp[,i]))[,2]))
+  nPlot(Freq ~ Var1, group = "Var2", data = data_freq,
+        type = "multiBarChart")
+  #p
+  plt[[index]]<-p
+  index<-index+1
+}
+plt
+i="Medical_Keyword"
+train_temp<-train_categ[,grep(i,colnames(train_categ))]
+index<-1
+plt<-list()
+for (i in colnames(train_temp)){
+  data_freq<-as.data.frame(table(train_temp[,i],train_clean$Response)/(as.data.frame(table(train_temp[,i]))[,2]))
+  nPlot(Freq ~ Var1, group = "Var2", data = data_freq,
+        type = "multiBarChart")
+  #p
+  plt[[index]]<-p
+  index<-index+1
+}
+plt
+
+i="Product_Info"
+if(class(train_clean[,grep(i, colnames(train_clean))])=="data.frame"){
+  hchart(cor(train_clean[,c(grep(i, colnames(train_clean)),119)]))
+}
+i="Employment_Info"
+if(class(train_clean[,grep(i, colnames(train_clean))])=="data.frame"){
+  hchart(cor(train_clean[,c(grep(i, colnames(train_clean)),119)]))
+}
+i="InsuredInfo_"
+if(class(train_clean[,grep(i, colnames(train_clean))])=="data.frame"){
+  hchart(cor(train_clean[,c(grep(i, colnames(train_clean)),119)]))
+}
+i="Insurance_History"
+if(class(train_clean[,grep(i, colnames(train_clean))])=="data.frame"){
+  hchart(cor(train_clean[,c(grep(i, colnames(train_clean)),119)]))
+}
+i="Medical_History"
+if(class(train_clean[,grep(i, colnames(train_clean))])=="data.frame"){
+  hchart(cor(train_clean[,c(grep(i, colnames(train_clean)),119)]))
+}
+
+### PCA
+
+train_apca<-data.frame(ID=train_clean$Id)
+i="Product_Info"
+mydata<-train_clean[,grep(i, colnames(train_clean))]
+mydata_test<-test_clean[,grep(i, colnames(test_clean))]
+pca_model<-prcomp(mydata, scale=T)
+variances <- pca_model$sdev^2  # variances
+pareto.chart (variances, ylab="Variances")  # plot pareto chart
+mydata_hat<-predict(pca_model, as.data.frame(mydata))
+nS <- nScree(x=eigen(cor(mydata))$values, aparallel=parallel(subject=nrow(mydata),                                        var=ncol(mydata))$eigen$qevpea)
+optimal_PCA<-nS$Components$noc
+train_apca<-cbind(train_apca,mydata_hat[,1:optimal_PCA])
+colnames(train_apca)[grepl("PC",colnames(train_apca))]<-paste0(i,1:optimal_PCA)
+plotnScree(nS)
+
+train_apca<-data.frame(ID=train_clean$Id)
+i="InsuredInfo_"
+mydata<-train_clean[,grep(i, colnames(train_clean))]
+mydata_test<-test_clean[,grep(i, colnames(test_clean))]
+pca_model<-prcomp(mydata, scale=T)
+variances <- pca_model$sdev^2  # variances
+pareto.chart (variances, ylab="Variances")  # plot pareto chart
+mydata_hat<-predict(pca_model, as.data.frame(mydata))
+nS <- nScree(x=eigen(cor(mydata))$values, aparallel=parallel(subject=nrow(mydata),                                        var=ncol(mydata))$eigen$qevpea)
+optimal_PCA<-nS$Components$noc
+train_apca<-cbind(train_apca,mydata_hat[,1:optimal_PCA])
+colnames(train_apca)[grepl("PC",colnames(train_apca))]<-paste0(i,1:optimal_PCA)
+plotnScree(nS)
+
+train_apca<-data.frame(ID=train_clean$Id)
+i="Insurance_History"
+mydata<-train_clean[,grep(i, colnames(train_clean))]
+mydata_test<-test_clean[,grep(i, colnames(test_clean))]
+pca_model<-prcomp(mydata, scale=T)
+variances <- pca_model$sdev^2  # variances
+pareto.chart (variances, ylab="Variances")  # plot pareto chart
+mydata_hat<-predict(pca_model, as.data.frame(mydata))
+nS <- nScree(x=eigen(cor(mydata))$values, aparallel=parallel(subject=nrow(mydata),                                        var=ncol(mydata))$eigen$qevpea)
+optimal_PCA<-nS$Components$noc
+train_apca<-cbind(train_apca,mydata_hat[,1:optimal_PCA])
+colnames(train_apca)[grepl("PC",colnames(train_apca))]<-paste0(i,1:optimal_PCA)
+plotnScree(nS)
+
+train_apca<-data.frame(ID=train_clean$Id)
+i="Medical_History"
+mydata<-train_clean[,grep(i, colnames(train_clean))]
+mydata_test<-test_clean[,grep(i, colnames(test_clean))]
+pca_model<-prcomp(mydata, scale=T)
+variances <- pca_model$sdev^2  # variances
+pareto.chart (variances, ylab="Variances")  # plot pareto chart
+mydata_hat<-predict(pca_model, as.data.frame(mydata))
+nS <- nScree(x=eigen(cor(mydata))$values, aparallel=parallel(subject=nrow(mydata),                                        var=ncol(mydata))$eigen$qevpea)
+optimal_PCA<-nS$Components$noc
+train_apca<-cbind(train_apca,mydata_hat[,1:optimal_PCA])
+colnames(train_apca)[grepl("PC",colnames(train_apca))]<-paste0(i,1:optimal_PCA)
+plotnScree(nS)
+
+i="Medical_Keyword"
+mydata<-train_clean[,grep(i, colnames(train_clean))]
+mydata_test<-test_clean[,grep(i, colnames(test_clean))]
+pca_model<-prcomp(mydata, scale=T)
+variances <- pca_model$sdev^2  # variances
+pareto.chart (variances, ylab="Variances")  # plot pareto chart
+mydata_hat<-predict(pca_model, as.data.frame(mydata))
+nS <- nScree(x=eigen(cor(mydata))$values, aparallel=parallel(subject=nrow(mydata),                                        var=ncol(mydata))$eigen$qevpea)
+optimal_PCA<-nS$Components$noc
+train_apca<-cbind(train_apca,mydata_hat[,1:optimal_PCA])
+colnames(train_apca)[grepl("PC",colnames(train_apca))]<-paste0(i,1:optimal_PCA)
+plotnScree(nS)
+
+temp1<- data.frame(Variable_Type = c("Product Information",
+                                     "Insurance Age",
+                                     "Height",
+                                     "Weight", 
+                                     "BMI",
+                                     "Employment Information",
+                                     "Insured Information",
+                                     "Insurance History",
+                                     "Family History",
+                                     "Medical History",
+                                     "Medical Keyword"))
+
+temp1$Prior_to_PCA<-c(7,1,1,1,1,6,7,7,1,37,48)
+temp1$After_PCA<-c(2,1,1,1,1,2,1,2,1,6,5)
+temp1[12,2:3]<-colSums(temp1[,-1])
+temp1$Variable_Type[12]<-"Total"
+datatable(temp1, options = list(pageLength = 13,
+                                initComplete = JS(
+                                  "function(settings, json) {",
+                                  "$(this.api().table().header()).css({'background-color': '#000', 'color': '#fff'});",
+                                  "}")))
